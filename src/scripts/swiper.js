978ade743e40a530d73eb5c63ff7fdd1ecb039b9
Diego Sanchez <diegosanchezsanchez18@gmail.com>
2025-04-12 10:04:24 -0500

    Fix FINAL Swiper funcional con DOMContentLoaded

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,21 +1,23 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  spaceBetween: 30,
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    640: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    1024: { slidesPerView: 3 },
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
 });
